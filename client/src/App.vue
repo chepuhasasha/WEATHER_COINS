791e89c425cfafdd636d96b458e328ad559aa0b3
Sergey Batukov <89929657+sashachepuha@users.noreply.github.com>
2021-11-13 19:07:53 +0300

    yarn

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,13 @@
 <template lang='pug'>
-h1 test
+  #app
+    h1 test
 </template>
 
 <script>
 export default {
   name: 'App',
-}
+};
 </script>
 
-<style lang='scss'>
-h1 {
-  color: $text;
-}
+<style lang="less">
 </style>
