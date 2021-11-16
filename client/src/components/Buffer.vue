ef055b76a443e99b86e04570827a8ca9417b7f89
Sergey Batukov <sashachepuha@gmail.com>
2021-11-16 07:11:38 +0000

    buffer

---
+++ client/src/components/Buffer.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  .buffer {{ getProfit.now }} {{ getProfit.buy }}
+</template>
+
+<script>
+export default {
+  name: 'Buffer',
+  props: {
+    profit: {
+      type: Number,
+      default: 0.01,
+    },
+    item: {
+      type: Object,
+      default: () => ({
+        id: '',
+        time: '',
+        tool: {
+          name: 'USD',
+          val: 100,
+        },
+        target: {
+          name: 'BTC',
+          val: 0.003,
+        },
+        commission: 0.02,
+        price: {
+          now: 43924,
+          buy: 44204,
+        },
+      }),
+    },
+  },
+  data: () => ({
+    minimize: false,
+  }),
+  computed: {
+    getProfit() {
+      const now = this.item.target.val * this.item.price.now;
+      const buy = this.item.target.val * this.item.price.buy;
+      return {
+        now,
+        buy,
+        status: '',
+      };
+    },
+  },
+};
+</script>
+
+<style></style>
