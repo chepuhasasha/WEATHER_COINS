833fd52ce94ba729eb22288f827f870f166467c0
Sergey Batukov <sashachepuha@gmail.com>
2021-11-16 07:21:44 +0000

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,21 @@
 <template lang="pug">
-#app
+#app(:style='getGrid')
   //- button(@click='Massage') NEW MASSAGE
-  Block(header='purse' area='1/1/3/3' :gap='0' :padding='0')
+  Block(header='purse' :area='layout.purse' :gap='0' :padding='0')
     Purse(name='BTC' val='0.13929')
     Purse(name='RUB' val='1 000 000')
     Purse(name='USD' val='30 000')
-  Block(header='Settings' area='3/1/5/3' load)
+  Block(header='Settings' :area='layout.settings' load)
     Button(text='Test')
-  Block(header='Block' area='1/3/5/11' :padding='0')
+  Block(header='Chart' :area='layout.chart' :padding='0')
     Chart
-  Block(header='Log' area='1/11/3/13')
+  Block(header='Buffer' :area='layout.buffer')
+    Buffer
+  Block(header='Log' :area='layout.log')
     Event
     Event
     Event
     Event
-  Block(header='Buffer' area='3/11/5/13' load)
 </template>
 
 <script>
@@ -26,18 +27,38 @@ export default {
     Chart: () => import('@/components/Chart.vue'),
     Event: () => import('@/components/Event.vue'),
     Purse: () => import('@/components/Purse.vue'),
+    Buffer: () => import('@/components/Buffer.vue'),
   },
   sockets: {
     connect() {
       console.log('CLIENT IO CONNECTED');
     },
   },
-  data() {
-    return {
-      something: [
-        // ... something here for the data if you need.
-      ],
-    };
+  data: () => ({
+    layout: {
+      grid: {
+        rows: 5,
+        cols: 12,
+        gap: 10,
+        padding: 10,
+      },
+      purse: '1/1/3/3',
+      settings: '3/1/5/3',
+      chart: '1/3/4/11',
+      buffer: '4/3/5/11',
+      log: '1/11/5/13',
+    },
+    something: [
+      // ... something here for the data if you need.
+    ],
+  }),
+  computed: {
+    getGrid: () => ({
+      'grid-template-columns': `repeat(${this.layout.grid.cols}, 1fr)`,
+      'grid-template-rows': `repeat(${this.layout.grid.rows}, 1fr)`,
+      gap: `${this.layout.grid.gap}px`,
+      padding: `${this.layout.grid.padding}px`,
+    }),
   },
   methods: {
     Massage() {
@@ -61,10 +82,6 @@ export default {
   height: 100vh;
 
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(4, 1fr);
-  gap: 10px;
-  padding: 20px;
 
   background: @bg_0;
 }
